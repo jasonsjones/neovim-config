86a9084f1360dae4abe6bcdd6a813bd1735e5583
Jason Jones <jsjones96@gmail.com>
2022-01-11 21:18:36 -0800

    chore: reformat lsp config files

--- lua/user/lsp/settings/jsonls.lua
+++ lua/user/lsp/settings/jsonls.lua
@@ -1,103 +1,103 @@
 -- Find more schemas here: https://www.schemastore.org/json/
 local schemas = {
-  {
-    description = "TypeScript compiler configuration file",
-    fileMatch = {
-      "tsconfig.json",
-      "tsconfig.*.json",
+    {
+        description = "TypeScript compiler configuration file",
+        fileMatch = {
+            "tsconfig.json",
+            "tsconfig.*.json",
+        },
+        url = "https://json.schemastore.org/tsconfig.json",
     },
-    url = "https://json.schemastore.org/tsconfig.json",
-  },
-  {
-    description = "Lerna config",
-    fileMatch = { "lerna.json" },
-    url = "https://json.schemastore.org/lerna.json",
-  },
-  {
-    description = "Babel configuration",
-    fileMatch = {
-      ".babelrc.json",
-      ".babelrc",
-      "babel.config.json",
+    {
+        description = "Lerna config",
+        fileMatch = { "lerna.json" },
+        url = "https://json.schemastore.org/lerna.json",
     },
-    url = "https://json.schemastore.org/babelrc.json",
-  },
-  {
-    description = "ESLint config",
-    fileMatch = {
-      ".eslintrc.json",
-      ".eslintrc",
+    {
+        description = "Babel configuration",
+        fileMatch = {
+            ".babelrc.json",
+            ".babelrc",
+            "babel.config.json",
+        },
+        url = "https://json.schemastore.org/babelrc.json",
     },
-    url = "https://json.schemastore.org/eslintrc.json",
-  },
-  {
-    description = "Prettier config",
-    fileMatch = {
-      ".prettierrc",
-      ".prettierrc.json",
-      "prettier.config.json",
+    {
+        description = "ESLint config",
+        fileMatch = {
+            ".eslintrc.json",
+            ".eslintrc",
+        },
+        url = "https://json.schemastore.org/eslintrc.json",
     },
-    url = "https://json.schemastore.org/prettierrc",
-  },
-  {
-    description = "Vercel Now config",
-    fileMatch = { "now.json" },
-    url = "https://json.schemastore.org/now",
-  },
-  {
-    description = "Stylelint config",
-    fileMatch = {
-      ".stylelintrc",
-      ".stylelintrc.json",
-      "stylelint.config.json",
+    {
+        description = "Prettier config",
+        fileMatch = {
+            ".prettierrc",
+            ".prettierrc.json",
+            "prettier.config.json",
+        },
+        url = "https://json.schemastore.org/prettierrc",
     },
-    url = "https://json.schemastore.org/stylelintrc",
-  },
-  {
-    description = "Json schema for properties json file for a GitHub Workflow template",
-    fileMatch = {
-      ".github/workflow-templates/**.properties.json",
+    {
+        description = "Vercel Now config",
+        fileMatch = { "now.json" },
+        url = "https://json.schemastore.org/now",
     },
-    url = "https://json.schemastore.org/github-workflow-template-properties.json",
-  },
-  {
-    description = "Packer template JSON configuration",
-    fileMatch = {
-      "packer.json",
+    {
+        description = "Stylelint config",
+        fileMatch = {
+            ".stylelintrc",
+            ".stylelintrc.json",
+            "stylelint.config.json",
+        },
+        url = "https://json.schemastore.org/stylelintrc",
     },
-    url = "https://json.schemastore.org/packer.json",
-  },
-  {
-    description = "NPM configuration file",
-    fileMatch = {
-      "package.json",
+    {
+        description = "Json schema for properties json file for a GitHub Workflow template",
+        fileMatch = {
+            ".github/workflow-templates/**.properties.json",
+        },
+        url = "https://json.schemastore.org/github-workflow-template-properties.json",
     },
-    url = "https://json.schemastore.org/package.json",
-  },
-  {
-    description = "JSON schema for Visual Studio component configuration files",
-    fileMatch = {
-      "*.vsconfig",
+    {
+        description = "Packer template JSON configuration",
+        fileMatch = {
+            "packer.json",
+        },
+        url = "https://json.schemastore.org/packer.json",
     },
-    url = "https://json.schemastore.org/vsconfig.json",
-  },
+    {
+        description = "NPM configuration file",
+        fileMatch = {
+            "package.json",
+        },
+        url = "https://json.schemastore.org/package.json",
+    },
+    {
+        description = "JSON schema for Visual Studio component configuration files",
+        fileMatch = {
+            "*.vsconfig",
+        },
+        url = "https://json.schemastore.org/vsconfig.json",
+    },
 }
 
 local opts = {
-  settings = {
-    json = {
-      schemas = schemas,
+    settings = {
+        json = {
+            schemas = schemas,
+        },
     },
-  },
-  setup = {
-    commands = {
-      Format = {
-        function()
-          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
-        end,
-      },
+    setup = {
+        commands = {
+            Format = {
+                function()
+                    vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
+                end,
+            },
+        },
     },
-  },
 }
 
 return opts
