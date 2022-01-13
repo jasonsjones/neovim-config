477f78217689f103df7b250a99fcd49880e9bf84
Jason Jones <jsjones96@gmail.com>
2022-01-12 22:00:17 -0800

    refactor: use 'vim.cmd' for autocommand

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,5 +1,3 @@
-local api = vim.api
-
 -- Remvoe trailing whitespace on save (for all files)
-api.nvim_exec([[ autocmd BufWritePre * :%s/\s\+$//e ]], false)
+vim.cmd([[ autocmd BufWritePre * :%s/\s\+$//e ]])
 
