645d934a6c9e15fc2c747afa1a87bcd20138c516
Jason Jones <jsjones96@gmail.com>
2022-01-20 17:59:10 -0800

    fix: update color of current line number in the gutter

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,3 +5,13 @@ if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not found.")
     return
 end
+
+-- Target event 'BufEnter' since the 'Colorscheme' event will get overridden
+-- when the actual colorscheme is loaded, which happens after the autocmd fires
+vim.cmd([[
+    augroup CustomColors
+        autocmd!
+        autocmd BufEnter * highlight CursorLineNR guifg=#ffe162
+    augroup end
+]])
+
