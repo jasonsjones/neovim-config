11216b2769086e5996d2378b9e59816782c1a0b8
Jason Jones <jsjones96@gmail.com>
2022-01-22 18:26:26 -0800

    feat: add autocommand for markdown file settings

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,3 +1,10 @@
 -- Remvoe trailing whitespace on save (for all files)
 vim.cmd([[ autocmd BufWritePre * :%s/\s\+$//e ]])
 
+vim.cmd([[
+    augroup Markdown
+        autocmd!
+        autocmd FileType markdown set wrap | set linebreak | set textwidth=80
+    augroup end
+]])
+
