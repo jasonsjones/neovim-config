a1682127a613186908b0b3788b542527c9543b06
Jason Jones <jsjones96@gmail.com>
2022-01-29 10:54:17 -0800

    fix: update nvim-cmp config

--- lua/user/complete.lua
+++ lua/user/complete.lua
@@ -106,7 +106,7 @@ cmp.setup {
         format = function(entry, vim_item)
             -- Kind icons
             vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
             vim_item.menu = ({
                 nvim_lsp = "[LSP]",
                 nvim_lua = "[NVIM_LUA]",
@@ -122,7 +122,7 @@ cmp.setup {
         { name = "nvim_lsp" },
         { name = "nvim_lua" },
         { name = "luasnip" },
-        { name = "buffer" },
+        { name = "buffer", keyword_length = 4 },
         { name = "path" },
     },
 
