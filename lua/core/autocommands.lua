32758c92bb1890c6b6ae57f142ae8c0e2bdb46bd
Jason Jones <jsjones96@gmail.com>
2023-01-15 17:49:42 -0800

    feat: [v2] add highlight on yank

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,12 +1,22 @@
 local augroup = vim.api.nvim_create_augroup
-local OrionGroup = augroup("Orion", {})
-
 local autocmd = vim.api.nvim_create_autocmd
+
+local OrionGroup = augroup("Orion", { clear = true })
+local HighlightGroup = augroup("YankHighlight", { clear = true })
 
 -- Remove trailing whitespace on save (for all files)
 autocmd({"BufWritePre"}, {
     group = OrionGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]]
+})
+
+-- Highlight on yank
+autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = HighlightGroup,
+    pattern = "*"
 })
 
