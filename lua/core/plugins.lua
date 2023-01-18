5caf7b9a6c452aab7c2ec203b51211e15d995523
Jason Jones <jsjones96@gmail.com>
2023-01-18 15:13:54 -0800

    feat: [v2] add minimal lsp support

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -47,6 +47,28 @@ return packer.startup(function(use)
     -- Treesitter for syntax highlighting and much more...
     use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
 
+    use {
+        "VonHeikemen/lsp-zero.nvim",
+        requires = {
+            -- LSP Support
+            {"neovim/nvim-lspconfig"},             -- Required
+            {"williamboman/mason.nvim"},           -- Optional
+            {"williamboman/mason-lspconfig.nvim"}, -- Optional
+
+            -- Autocompletion
+            {"hrsh7th/nvim-cmp"},         -- Required
+            {"hrsh7th/cmp-nvim-lsp"},     -- Required
+            {"hrsh7th/cmp-buffer"},       -- Optional
+            {"hrsh7th/cmp-path"},         -- Optional
+            {"saadparwaiz1/cmp_luasnip"}, -- Optional
+            {"hrsh7th/cmp-nvim-lua"},     -- Optional
+
+            -- Snippets
+            {"L3MON4D3/LuaSnip"},             -- Required
+            {"rafamadriz/friendly-snippets"}, -- Optional
+        }
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
