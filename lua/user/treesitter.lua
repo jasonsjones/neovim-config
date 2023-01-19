f66137403f676f26290034ac47685a1778b4d828
Jason Jones <jsjones96@gmail.com>
2023-01-18 18:27:29 -0800

    refactor: [v2] prep init.lua for dev branch merge

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-    ensure_installed = "all",
+    ensure_installed = { "help", "ledger", "lua" },
     sync_install = false,
     ignore_install = { "" }, -- List of parsers to ignore installing
     autopairs = {
