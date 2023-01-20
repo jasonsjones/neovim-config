45986cae7b3a9ae5ca4653775f7ec4567ed570e7
Jason Jones <jsjones96@gmail.com>
2023-01-19 18:58:35 -0800

    feat: [v2] update remaps

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -13,9 +13,17 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
+
 --    Insert     --
 -- Use 'kj' to enter normal mode
 vim.keymap.set("i", "kj", "<ESC>", opts)
+
+-- Undo break points
+vim.keymap.set("i", ",", ",<c-g>u", opts)
+vim.keymap.set("i", ".", ".<c-g>u", opts)
+vim.keymap.set("i", "!", "!<c-g>u", opts)
+vim.keymap.set("i", "?", "?<c-g>u", opts)
+
 
 --    Normal     --
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, { desc = "[F]ile [E]xplorer" })
@@ -30,4 +38,28 @@ vim.keymap.set ("n", "<C-l>", "<C-w>l", opts)
 vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
 vim.keymap.set("n", "<leader>bd", ":Bdelete!<CR>", opts)
+
+
+--    Visual     --
+-- Press kj fast to enter normal mode
+vim.keymap.set("v", "kj", "<ESC>", opts)
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Move text up and down
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==", opts)
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==", opts)
+
+-- Retain yanked text after paste
+vim.keymap.set("v", "p", '"_dP', opts)
+
+
+--    Visual Block     --
+-- Move text up and down
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", opts)
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
