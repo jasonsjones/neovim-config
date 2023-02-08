14f310967fafeea6192feadcc6e2075ea51852ed
Jason Jones <jsjones96@gmail.com>
2023-02-07 18:39:07 -0800

    feat: add telescope keymap for buffers

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -23,4 +23,5 @@ vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles
 vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 vim.keymap.set("n", "<leader>sw", builtin.grep_string,  { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sb", builtin.buffers,  { desc = "[S]earch bB]uffers" })
 
